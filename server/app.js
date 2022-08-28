d497f489a40ebdfec3a228b5a235cd901cd1ade8
Yasser Bal <yasserbal@icloud.com>
2022-08-28 11:39:54 +0200

    Pre-Github-Link-Commit

--- server/app.js
+++ server/app.js
@@ -1,27 +1,40 @@
 const createError = require("http-errors");
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-
+//Req Session
+const session = require("express-session");
 //IMPORT ROUTERS
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
 const app = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+let sessionStore = [];
 //SET MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "Shshshhss",
+    resave: false,
+    saveUninitialized: true,
+    sessionStore: sessionStore,
+    cookie: {
+      hello: "I am a cookie!",
+    },
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -41,5 +54,4 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
 module.exports = app;
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,15 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+//MiddleWare example
+function middleware(req, res, next) {
+  console.log("Logged @ login.js");
+  console.log("Middleware Example");
+  const { username, password } = req.body;
+  console.table({
+    usernameReceived: username,
+    passwordReceived: password,
+  });
+  next();
+}
+//Errorhandler example
+function errorHandler(err, req, res, next) {
+  console.log("Logged @ login.js");
+  console.log("Errorhandler Example");
+  next();
+}
+//Say Hello from this route
 router.get("/", function (req, res, next) {
   res.send("Message From Login! :D");
 });
 
-//Frontend Connection
-router.post("/", (req, res, next) => {
+//Frontend Connection test
+router.post("/", middleware, errorHandler, (req, res, next) => {
   const { username, password } = req.body;
-  console.log(`received:${username}`);
+  if (!username && password) {
+    res.status(400);
+  }
   res.send({
     message: "Request received at /login",
     usernameReceived: username,
