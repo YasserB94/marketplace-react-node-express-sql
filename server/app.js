326c9f4be0ea2f498cadfda3e8a0c9f69dd87916
Yasser Bal <yasserbal@icloud.com>
2022-08-29 13:22:19 +0200

    Setup CORS - Session now properly workign with client

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-//Session stuff
+//Session stuff - Options to connect SQL store to DB -> MariaDB pool is not working - Bumped existing github issue
 const options = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -14,9 +14,9 @@ const options = {
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
 };
-const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
-const sessionStore = new MySQLStore(options);
+const session = require("express-session"); //Get Session
+const MySQLStore = require("express-mysql-session")(session); //Get MySQL store class
+const sessionStore = new MySQLStore(options); //Create store (creates new db connection pool with max conn set to 1  by default)
 
 //IMPORT ROUTERS
 const indexRouter = require("./routes/index");
@@ -30,21 +30,34 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+//Cors stuff
+//Define allowed origins
+const origins = ["http://localhost:3000", "http://localhost:3050"];
+const corsOptions = {
+  origin: origins, //Pass in allowed origins array
+  methods: ["GET", "POST"],
+  optionsSuccessStatus: 200, // ???
+  //withCredentials: true, // Could help to get cookie credentials -> Not making a differenc eon startup
+  credentials: true, //--> This option allows client to identify with cookie!
+};
+app.use(cors(corsOptions)); //Make App use cors with above options
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+//Make app Use defined session
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
+    secret: process.env.SESSION_SECRET, //Loading Session secret from dotenv
+    resave: false, //No need to save again if nothign changed ???
+    saveUninitialized: true, //Save the session even if there is no data supplied
+    store: sessionStore, //Use above defined SQL store to save session stuff (This way it persists if server crashes)
     cookie: {
+      //Save session in a cookie thats max x age
       maxAge: 1000 * 60 * 60 * 24, //This is one day,
     },
   })
 );
-
+//Routers
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
@@ -53,7 +66,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+//TODO:: Optimise error handler currently using express's built in one
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
